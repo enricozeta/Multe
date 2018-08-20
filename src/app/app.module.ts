fef3c1a074b3da0cbaa0b2646d9a5ab0db52f79c
Enrico Zamberlan <enrico.zamberlan@h-farm.com>
2018-08-21 00:42:48 +0200

    Test angular material

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,31 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {MatButtonModule, MatCheckboxModule, MatSidenavModule, MatListModule, MatFormFieldModule, MatSelectModule,
+  MatInputModule, MatTableModule} from '@angular/material';
 import { AppComponent } from './app.component';
+import { MenuComponent } from './menu/menu.component';
+import { NewPlayerComponent } from './new-player/new-player.component';
+import { PlayersComponent } from './players/players.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuComponent,
+    NewPlayerComponent,
+    PlayersComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatButtonModule,
+    MatCheckboxModule,
+    MatSidenavModule,
+    MatListModule,
+    MatFormFieldModule,
+    MatSelectModule,
+    MatInputModule,
+    MatTableModule
   ],
   providers: [],
   bootstrap: [AppComponent]
