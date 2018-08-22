2dd60b21f9f140dd5d6bf61aee757df2f51a7525
Enrico Zamberlan <enrico.zamberlan@h-farm.com>
2018-08-22 14:31:29 +0200

    Modify the players table

--- src/app/players/players.component.ts
+++ src/app/players/players.component.ts
@@ -4,20 +4,20 @@ import {MatTableDataSource} from '@angular/material';
 export interface PeriodicElement {
   name: string;
   position: number;
-  weight: number;
-  symbol: string;
+  daPagare: number;
+  pagate: string;
 }
 const ELEMENT_DATA: PeriodicElement[] = [
-  {position: 1, name: 'Hydrogen', weight: 1.0079, symbol: 'H'},
-  {position: 2, name: 'Helium', weight: 4.0026, symbol: 'He'},
-  {position: 3, name: 'Lithium', weight: 6.941, symbol: 'Li'},
-  {position: 4, name: 'Beryllium', weight: 9.0122, symbol: 'Be'},
-  {position: 5, name: 'Boron', weight: 10.811, symbol: 'B'},
-  {position: 6, name: 'Carbon', weight: 12.0107, symbol: 'C'},
-  {position: 7, name: 'Nitrogen', weight: 14.0067, symbol: 'N'},
-  {position: 8, name: 'Oxygen', weight: 15.9994, symbol: 'O'},
-  {position: 9, name: 'Fluorine', weight: 18.9984, symbol: 'F'},
-  {position: 10, name: 'Neon', weight: 20.1797, symbol: 'Ne'},
+  {position: 1, name: 'Hydrogen', daPagare: 1.0079, pagate: 'H'},
+  {position: 2, name: 'Helium', daPagare: 4.0026, pagate: 'He'},
+  {position: 3, name: 'Lithium', daPagare: 6.941, pagate: 'Li'},
+  {position: 4, name: 'Beryllium', daPagare: 9.0122, pagate: 'Be'},
+  {position: 5, name: 'Boron', daPagare: 10.811, pagate: 'B'},
+  {position: 6, name: 'Carbon', daPagare: 12.0107, pagate: 'C'},
+  {position: 7, name: 'Nitrogen', daPagare: 14.0067, pagate: 'N'},
+  {position: 8, name: 'Oxygen', daPagare: 15.9994, pagate: 'O'},
+  {position: 9, name: 'Fluorine', daPagare: 18.9984, pagate: 'F'},
+  {position: 10, name: 'Neon', daPagare: 20.1797, pagate: 'Ne'},
 ];
 
 @Component({
@@ -27,7 +27,7 @@ const ELEMENT_DATA: PeriodicElement[] = [
 })
 export class PlayersComponent implements OnInit {
 
-  displayedColumns: string[] = ['position', 'name', 'weight', 'symbol'];
+  displayedColumns: string[] = ['position', 'name', 'daPagare', 'pagate'];
   dataSource = new MatTableDataSource(ELEMENT_DATA);
 
   applyFilter(filterValue: string) {
