bc92aa6272d52b110263d5e0e52f97c27f00e5a8
Enrico Zamberlan <enrico.zamberlan@h-farm.com>
2019-01-16 16:59:24 +0100

    test build

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
-export const env = {
+export const environment = {
   production: true,
   baseUrl: 'https://multe.herokuapp.com'
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,7 +2,7 @@
 // `ng build ---prod` replaces `environment.ts` with `environment.prod.ts`.
 // The list of file replacements can be found in `angular.json`.
 
-export const env = {
+export const environment = {
   production: false,
 
   baseUrl: 'https://multe.herokuapp.com'
